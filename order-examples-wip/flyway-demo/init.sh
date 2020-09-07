4b2e55e043e81aae34c39d691ccba5e7b51b411d
Alexander Skjolden <alex@plastboks.net>
2020-09-07 16:05:04 +0200

    simplifies init.sh even furhter

--- order-examples-wip/flyway-demo/init.sh
+++ order-examples-wip/flyway-demo/init.sh
@@ -5,10 +5,4 @@ co-pilot download cli
 
 # This should be the only thing needed
 co-pilot spring init --config-file co-pilot.info
-
-# "co-pilot spring install --config-file co-pilot.info.next.level" should replace the bash below 
-template_dir=~/.co-pilot/cloud-config/templates/flyway-demo
-
 co-pilot merge template --name flyway-demo --target webservice
-co-pilot merge pom  --from $template_dir/pom.xml --target webservice
-co-pilot merge text --from $template_dir/src/main/resources/application.properties --to webservice/src/main/resources/application.properties
