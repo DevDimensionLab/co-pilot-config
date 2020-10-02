259465f9ab84468a07b814390db62c5272fbe1d2
Alexander Skjolden <alex@plastboks.net>
2020-10-02 20:46:31 +0200

    Prepares for v0.3.0

--- test/co-pilot-orders-test.sh
+++ test/co-pilot-orders-test.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
+shopt -s inherit_errexit
+
 for f in $(find test -name "co-pilot.json"); do
 	dir=$(dirname $f)
 	pom="$dir/pom.xml"
 	if [[ ! -f "$pom" ]]; then # only folders which does NOT contain pom.xml
-		co-pilot spring init --config-file $f --target "$dir/app"
+		co-pilot generate --config-file $f --target "$dir/app"
+		cd "$dir/app"
+		mvn clean install
+		cd -
 	fi
 done
--- test/order/rest-basic/init.sh
+++ test/order/rest-basic/init.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-co-pilot spring init --config-file co-pilot.json --target app
+co-pilot generate --config-file co-pilot.json --target app
