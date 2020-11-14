03f744be150a46de43e34afd60a6508d3a8d07ce
Thomas Andersen <thomas.andersen@knowit.no>
2020-11-14 15:56:03 +0100

    added logback

--- maven-copilot-plugin/src/main/java/no/co_pilot/maven/prop/WriteApplicationPropertiesMojo.java
+++ maven-copilot-plugin/src/main/java/no/co_pilot/maven/prop/WriteApplicationPropertiesMojo.java
@@ -1,11 +1,16 @@
 package no.co_pilot.maven.prop;
 
+import org.apache.maven.model.Dependency;
+import org.apache.maven.model.Model;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Component;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
+
+import java.util.List;
 
 @Mojo(name = "extractDependencyVersions", defaultPhase = LifecyclePhase.COMPILE)
 public class WriteApplicationPropertiesMojo extends AbstractMojo {
@@ -13,7 +18,11 @@ public class WriteApplicationPropertiesMojo extends AbstractMojo {
     @Parameter(required = true)
     private String pomXml;
 
+    @Component
+    private Model model;
+
     public void execute() throws MojoExecutionException, MojoFailureException {
+        List<Dependency> dependencies = model.getDependencies();
 
     }
 }
--- maven-copilot-plugin/src/test/java/no/co_pilot/maven/prop/WriteApplicationPropertiesMojoTest.java
+++ maven-copilot-plugin/src/test/java/no/co_pilot/maven/prop/WriteApplicationPropertiesMojoTest.java
@@ -1,5 +1,54 @@
-import static org.junit.jupiter.api.Assertions.*;
+package no.co_pilot.maven.prop;
 
-class WriteApplicationPropertiesMojoTest {
+import org.apache.maven.plugin.Mojo;
+import org.apache.maven.plugin.testing.AbstractMojoTestCase;
+import org.apache.maven.plugin.testing.MojoRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.BlockJUnit4ClassRunner;
 
+import java.io.File;
+
+import static org.codehaus.plexus.PlexusTestCase.getTestFile;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+@RunWith(BlockJUnit4ClassRunner.class)
+public class WriteApplicationPropertiesMojoTest extends AbstractMojoTestCase {
+
+/*    @Rule
+    public MojoRule rule = new MojoRule(this)
+    {
+        @Override
+        protected void before()
+        {
+        }
+
+        @Override
+        protected void after()
+        {
+        }
+    };*/
+
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    @Test
+    public void testMojo() throws Exception {
+        File pom = getTestFile("src/main/java/resources/pom.xml");
+
+        Mojo mojo = lookupEmptyMojo("install", pom);
+        assertNotNull(mojo);
+        assertTrue(pom.exists());
+        mojo.execute();
+    }
 }
